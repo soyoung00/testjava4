42e80a7794fd65fbb77da1c2b70b55d4e2bd9d90
soyoung00 <parksy9750@naver.com>
2020-08-13 15:27:21 +0900

    step1

---
+++ Test22.java
@@ -0,0 +1,19 @@
+Test22 class
+public class Test22{
+	public static void main(String[] args){
+		int a = 12;
+		init b = 2;
+		car op = '+';
+
+	switch(op){
+		case '+':
+			System.out.println(a+b); break;
+		case '-':
+			System.out.println(a-b); break;
+		case '*':
+			System.out.println(a*b); break;
+		case '/':
+			System.out.println(a/b); break;
+		}
+	}
+}
