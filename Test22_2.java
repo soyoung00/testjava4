598032f16b2e25e49f1c2ec23ca3dfb3968823e4
soyoung00 <parksy9750@naver.com>
2020-08-13 15:33:58 +0900

    step2

---
+++ Test22_2.java
@@ -0,0 +1,13 @@
+Test22_2.java;
+public class Test22_2{
+	public static void main(String[] args){
+		int a=10;
+		int b=1;
+		char op='+';
+
+		switch(op){
+			case '+':
+				System.out.println(a+b); break;
+		}
+	}
+}
